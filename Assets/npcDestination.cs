9dac28ee0094164461a42ac890064be539ab672e
Hitman <ivan.kojundzic5@gmail.com>
2023-03-08 13:02:45 +0100

    upload

--- Assets/npcDestination.cs
+++ Assets/npcDestination.cs
@@ -17,17 +17,17 @@ public class npcDestination : MonoBehaviour
          }
          if (pivotPoint == 2)
          {
-            this.gameObject.transform.position= new Vector3(-0.26f,-0.037f,-7.47f);
+            this.gameObject.transform.position= new Vector3(-183.7338f,17.302f,0.724f);
             pivotPoint = 3;
          }
          if (pivotPoint == 1)
          {
-            this.gameObject.transform.position= new Vector3(-3.62f,-0.037f,-11.8f);
+            this.gameObject.transform.position= new Vector3(-165.15f,19.69f,2.611f);
             pivotPoint = 2;
          }
          if (pivotPoint == 0)
          {
-            this.gameObject.transform.position= new Vector3(2.45f,-0.037f,-16.56f);
+            this.gameObject.transform.position= new Vector3(-165.15f,19.69f,9.3f);
             pivotPoint = 1;
          }
         
