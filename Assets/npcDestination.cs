74f9ce053d352ab20a1fea93e8b2516e71c855a9
Hitman <ivan.kojundzic5@gmail.com>
2023-03-13 10:23:24 +0100

    upload

--- Assets/npcDestination.cs
+++ Assets/npcDestination.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class npcDestination : MonoBehaviour
 {
    public int pivotPoint;
+   public GameObject destination;
 
    private void OnTriggerEnter(Collider other)
    {
@@ -13,21 +14,21 @@ public class npcDestination : MonoBehaviour
       {
          if (pivotPoint == 3)
          {
-            pivotPoint = 0;
+            pivotPoint = 1;
          }
          if (pivotPoint == 2)
          {
-            this.gameObject.transform.position= new Vector3(-183.7338f,17.302f,0.724f);
+            destination.gameObject.transform.position= new Vector3(-183.7338f,17.302f,0.724f);
             pivotPoint = 3;
          }
          if (pivotPoint == 1)
          {
-            this.gameObject.transform.position= new Vector3(-165.15f,19.69f,2.611f);
+            destination.gameObject.transform.position= new Vector3(-165.15f,19.69f,1f);
             pivotPoint = 2;
          }
          if (pivotPoint == 0)
          {
-            this.gameObject.transform.position= new Vector3(-165.15f,19.69f,9.3f);
+            destination.gameObject.transform.position= new Vector3(-165.15f,19.69f,9.3f);
             pivotPoint = 1;
          }
         
